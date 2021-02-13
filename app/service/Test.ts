f338026786c033cfad08ac8df26b301d0fc8e025
12144 <1214425528@qq.com>
2021-02-13 10:25:35 +0800

    配置mysql

---
+++ app/service/Test.ts
@@ -0,0 +1,18 @@
+import { Service } from 'egg';
+
+/**
+ * Test Service
+ */
+export default class Test extends Service {
+
+  /**
+   * sayHi to you
+   * @param name - your name
+   */
+  public async sayHi(name: string) {
+    return `hi, ${name}`;
+  }
+  async status() {
+    return this.app.mysql.select('test')
+  }
+}
